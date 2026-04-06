824fdf2c703677408a4557939a1b3af2397c2404
AnhNND <nducanh381999@gmail.com>
2026-04-06 17:44:56 +0700

    Fix UI

--- Quay27.Infrastructure/Repositories/CustomerRepository.cs
+++ Quay27.Infrastructure/Repositories/CustomerRepository.cs
@@ -40,10 +40,11 @@ public class CustomerRepository : ICustomerRepository
         if (queueId is not null)
         {
             query = query.Where(c => c.CustomerQueues.Any(cq => cq.QueueId == queueId));
-            // if (queueId == SchemaConstants.Quay27QueueId)
-            // {
-            //     query = query.Where(c => !c.Export27);
-            // }
+            if (queueId == SchemaConstants.Quay27QueueId)
+            {
+                var limitDate = DateTime.UtcNow.AddDays(-30);
+                query = query.Where(c => c.BillCreatedAt >= limitDate);
+            }
         }
 
         var rows = queueId is null
